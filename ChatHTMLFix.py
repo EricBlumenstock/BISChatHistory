874fad8679261a011d01b75489fc4dadd457bb8d
Eric Blumenstock <EricBlumenstock@users.noreply.github.com>
2017-11-17 14:47:56 -0500

    Pretty much finished.
    Could use some arg changes though. Redacted sensitive information.

--- ChatHTMLFix.py
+++ ChatHTMLFix.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pymysql as sql
 import datetime as dt
 
@@ -10,37 +11,42 @@ def replace(item: str, tokens: dict) -> str:
 
     return item
 
-def fileFix(path):
+def main():
+    tokens = {'&lt;': '<', '&gt;': '>', '<>': '', '",), ("': '<hr>', ',), ("': '<hr>', """",), ('""": '<hr>', '(("': '',  '",))': ''}
+
+    with sql.connect(host='HOST', port=3306, user='USERNAME', passwd='PASSWORD', db='mysql') as db:
+
+        db.execute("""SELECT formatted FROM PHPLive_ChatV4.p_transcripts
+        WHERE deptID IN (SELECT deptID FROM PHPLive_ChatV4.p_dept_ops WHERE opid = 28)
+        ORDER BY created desc""")
+
+        w = str(db.fetchmany(int( (sys.argv[1] if len(sys.argv) >= 2 else 10 ) )))
+
+    w = replace(w, tokens)
+
+    with open(os.path.expanduser("~/Desktop/" + str(dt.date.today()) + ' ChatHist.html'), mode='w', encoding='utf8') as file:
+        file.writelines(w)
+
+
+main()
+
+
+
+
+
+#Unused Function
+#For fixing a file provided as an argument
+#fileFix(sys.argv[1])
+def fileFix(path: str):
 
     tokens = {'&lt;': '<', '&gt;': '>', '<>': ''}
     w = list()
 
-    with open(path, mode='r') as file:
+    with open(path, mode='r', encoding='utf8') as file:
 
         for line in file:
             line = replace(line, tokens)
             w.append(line)
 
-    with open(path, mode='w') as file:
+    with open(path, mode='w', encoding='utf8') as file:
         file.writelines(w)
-
-
-def main():
-
-    #TODO create connection
-    with sql.connect(host='vm-database', port=3306, user='', passwd='', db='mysql') as db:
-        cur = db.cursor()
-
-        #TODO
-        cur.execute('')
-
-        w = list(cur.fetchall())
-
-    for record in w:
-        record = replace(record, tokens)
-
-    with open(str(dt.date) + ' ChatHist', mode='w') as file:
-        file.writelines(w)
-
-
-main()
